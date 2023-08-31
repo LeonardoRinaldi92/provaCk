0b10d53073f958917b1675c576f38bc35659856d
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-31 13:26:55 +0200

    struttura aggiornata

---
+++ app/Http/Controllers/SodaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Soda;
+use Illuminate\Http\Request;
+
+class SodaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Soda  $soda
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Soda $soda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Soda  $soda
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Soda $soda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Soda  $soda
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Soda $soda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Soda  $soda
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Soda $soda)
+    {
+        //
+    }
+}
