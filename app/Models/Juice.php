0f452575317b6ec670dcd78e5edb261d3c0fde6e
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-31 12:49:46 +0200

    c'è da modificare 2

--- app/Models/Juice.php
+++ app/Models/Juice.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class juices extends Model
+class Juice extends Model
 {
     use HasFactory;
 
