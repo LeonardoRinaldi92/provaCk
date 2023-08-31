3e30d38dec2596d690fe158349fe781ffe5d77e7
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-31 18:27:02 +0200

    aggiunto seeder per bitter

--- database/seeders/BitterSeeder.php
+++ database/seeders/BitterSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Bitter;
+use App\Models\Ingredient;
 class BitterSeeder extends Seeder
 {
     /**
@@ -14,6 +16,21 @@ class BitterSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Crea alcuni esempi di alcools
+        $Angostura = Bitter::create(['name' => 'Angostura','ABV' => '45']);
+        // Aggiungi altri alcools se necessario
+        
+        // Crea i record corrispondenti nella tabella ingredients
+        $this->createIngredient($Angostura);
+    }
+
+    protected function createIngredient($bitter)
+    {
+        $ingredient = new Ingredient([
+            'name' => $bitter->name,
+            'ingredientable_type' => $bitter->getTable(),
+            'ingredientable_id' => $bitter->id,
+        ]);
+        $ingredient->save();
     }
 }
