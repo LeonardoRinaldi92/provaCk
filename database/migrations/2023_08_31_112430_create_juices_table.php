937283f94f66ab5afca9ccbf19602a3e127993f7
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-09-06 15:35:54 +0200

    tolta descrizione da juices

--- database/migrations/2023_08_31_112430_create_juices_table.php
+++ database/migrations/2023_08_31_112430_create_juices_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('juices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
