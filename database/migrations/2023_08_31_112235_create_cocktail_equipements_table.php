5ad4a79928c872ad90b182b9ae14415bb3f83abb
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-09-06 11:08:21 +0200

    corretta struttra tabella pivot

--- database/migrations/2023_08_31_112235_create_cocktail_equipements_table.php
+++ database/migrations/2023_08_31_112235_create_cocktail_equipements_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cocktail_equipements', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('cocktail_id');
             $table->foreign('cocktail_id')->references('id')->on('cocktails')->onDelete('cascade');
             $table->unsignedBigInteger('equipement_id');
             $table->foreign('equipement_id')->references('id')->on('equipements')->onDelete('cascade');
             $table->timestamps();
+            
+            // Definire la chiave primaria composta
+            $table->primary(['cocktail_id', 'equipement_id']);
         });
     }
 
